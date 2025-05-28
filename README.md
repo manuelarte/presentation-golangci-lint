e86fc71da828524aee99abbce78503a19f02a5cb
manuelarte <manuelarte@users.noreply.github.com>
2025-05-28 21:35:53 +0000

    deploy: e631c06598caa65af74679135cf42998c06ffd50

---
+++ README.md
@@ -0,0 +1,22 @@
+# Presentation `golangci-lint`
+
+Here I have the content to give the presentation about [golangci-lint][1]
+
+## Tools
+
+- I am using [reveal.js][2] to create the slides.
+- I am using [Jujutsu][3] as a version control.
+
+```bash
+npm start
+```
+
+## TODO
+
+- Improve slides
+- Add code with running with custom linters
+  - linter to check that all the json are camelCase
+
+[1]: https://golangci-lint.run/
+[2]: https://marp.app/
+[3]: https://github.com/jj-vcs/jj
---
+++ unexportedconstantscheck/analyzer.go
@@ -0,0 +1,63 @@
+package unexportedconstantscheck
+
+import (
+	"fmt"
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+func NewAnalyzer() *analysis.Analyzer {
+	return &analysis.Analyzer{
+		Name:     "unexportedconstantscheck",
+		Doc:      "unexportedconstantscheck checks if unexported constants starts with _",
+		Run:      run,
+		Requires: []*analysis.Analyzer{inspect.Analyzer},
+	}
+}
+
+func run(pass *analysis.Pass) (any, error) {
+	insp, found := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	if !found {
+		//nolint:nilnil // impossible case.
+		return nil, nil
+	}
+
+	nodeFilter := []ast.Node{
+		(*ast.ValueSpec)(nil),
+	}
+
+	insp.Preorder(nodeFilter, func(n ast.Node) {
+		if valueSpec, ok := n.(*ast.ValueSpec); ok {
+			// TODO(manuelarte): to be done
+			println(valueSpec)
+			// End TODO(manuelarte)
+		}
+	})
+
+	//nolint:nilnil //any, error
+	return nil, nil
+}
+
+func newUnexportedConstantsCheckDiag(i *ast.Ident) analysis.Diagnostic {
+	msg := fmt.Sprintf("unexported constant %q should be prefixed with _",
+		i.Name)
+
+	return analysis.Diagnostic{
+		Pos:     i.Pos(),
+		Message: msg,
+		SuggestedFixes: []analysis.SuggestedFix{
+			{
+				Message: "unexported constant %q should be prefixed with _",
+				TextEdits: []analysis.TextEdit{
+					{
+						Pos:     i.Pos(),
+						NewText: []byte(fmt.Sprintf("_%s", i.Name)),
+					},
+				},
+			},
+		},
+	}
+}
